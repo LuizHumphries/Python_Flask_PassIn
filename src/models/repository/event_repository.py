84c9fbcff123b63b88db160a7b651c1dc84ec808
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-04-03 17:26:24 -0300

    improve: Addind try except login in events_repository

--- src/models/repository/event_repository.py
+++ src/models/repository/event_repository.py
@@ -1,30 +1,40 @@
 from typing import Dict
 from src.models.settings.connection import db_connection_handler
 from src.models.entities.events import Events
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 
 class EventRepository:
 
     def insert_event(self, events_info: Dict) -> Dict:
         with db_connection_handler as database:
-            event = Events(
-                id=events_info.get("uuid"),
-                title=events_info.get("title"),
-                details=events_info.get("details"),             
-                slug=events_info.get("slug"),           
-                maximum_attendees=events_info.get("maximum_attendees")        
-            )
-            database.session.add(event)
-            database.session.commit()
-            return events_info
+            try:
+                event = Events(
+                    id=events_info.get("uuid"),
+                    title=events_info.get("title"),
+                    details=events_info.get("details"),             
+                    slug=events_info.get("slug"),           
+                    maximum_attendees=events_info.get("maximum_attendees")        
+                )
+                database.session.add(event)
+                database.session.commit()
+                return events_info
+            except IntegrityError:
+                raise Exception("Event already in database")
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
     
     def get_event_by_id(self, event_id: str) -> Events:
         with db_connection_handler as database:
-            event = (
-                database.session
-                    .query(Events)
-                    .filter(Events.id==event_id)
-                    .one()
-            )
-            return event
-
+            try:
+                event = (
+                    database.session
+                        .query(Events)
+                        .filter(Events.id==event_id)
+                        .one()
+                )
+                return event
+            except NoResultFound:
+                return None
+            
--- src/models/repository/event_repository_test.py
+++ src/models/repository/event_repository_test.py
@@ -7,18 +7,19 @@ db_connection_handler.connect_to_db()
 @pytest.mark.skip(reason="New register into database")
 def test_insert_event():
     event = {
-        "uuid": "my-uuid-test-2",
+        "uuid": "my-uuid-test-3",
         "title": "My Title",
-        "slug": "slug-data-2",
+        "slug": "slug-data-3",
         "maximum_attendees": 20
     }
     event_repository = EventRepository()
     response = event_repository.insert_event(event)
     print(response)
 
-@pytest.mark.skip(reason="Not necessary")
+#@pytest.mark.skip(reason="Not necessary")
 def test_get_event_by_id():
-    event_id = "my-uuid-test-2"
+    event_id = "my-uuid-test-26565"
     event_repository = EventRepository()
     response = event_repository.get_event_by_id(event_id=event_id)
     print(response)
+    
