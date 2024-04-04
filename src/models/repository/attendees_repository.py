e250df7bd02e8df6836f8370d16e5d6f20fe61bd
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-04-03 21:04:41 -0300

    feat: Implementing get attendees by Event

--- src/models/repository/attendees_repository.py
+++ src/models/repository/attendees_repository.py
@@ -1,7 +1,8 @@
-from typing import Dict
+from typing import Dict, List
 from src.models.settings.connection import db_connection_handler
 from src.models.entities.events import Events
 from src.models.entities.attendees import Attendees
+from src.models.entities.check_ins import CheckIns
 from sqlalchemy.exc import NoResultFound, IntegrityError
 
 
@@ -14,7 +15,7 @@ class AttendeesRepository:
                     id=attendees_info.get("uuid"),
                     name=attendees_info.get("name"),
                     email=attendees_info.get("email"),             
-                    event_id=attendees_info.get("event_id")       
+                    event_id=attendees_info.get("event_id")     
                 )
                 database.session.add(attendees)
                 database.session.commit()
@@ -44,3 +45,20 @@ class AttendeesRepository:
             except NoResultFound:
                 return None
             
+    def get_attendees_by_event_id(self, event_id:str) -> List[Attendees]:
+        with db_connection_handler as database:
+            attendees = (
+                database.session
+                    .query(Attendees)
+                    .outerjoin(CheckIns, CheckIns.attendeeId == Attendees.id)
+                    .filter(Attendees.event_id == event_id)
+                    .with_entities(
+                        Attendees.id,
+                        Attendees.name,
+                        Attendees.email,
+                        CheckIns.created_at.label("checked_in_at"),
+                        Attendees.created_at.label("attendees_created_at")
+                    )
+                    .all()
+            )
+            return attendees
